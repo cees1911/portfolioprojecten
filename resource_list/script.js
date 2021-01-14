77808ba3520d9daa07019a74910dad5608162dbc
Cees Klap <ceesklap@gmail.com>
2021-01-14 08:06:03 +0100

    made the promgram more DRY removed 2 lines

--- resource_list/script.js
+++ resource_list/script.js
@@ -1,7 +1,3 @@
-const radioButton = document.querySelectorAll(".selectButton");
-const getUl = document.getElementById("infoList");
-
-//ophalen van de data   190c4e4a-3b0d-46f9-98a4-314aa77804f5 
 function fetchData(choice) {
   fetch("https://api.npoint.io/db2ee5cd92ca82a22b27")
     .then((res) => res.json())
@@ -17,17 +13,17 @@ function fetchData(choice) {
 function pushDataInDom(logo, info) {
   document.getElementById("logoImage").src = `../images/${logo}.png`;
 
-  getUl.innerHTML = "";
+  document.getElementById("infoList").innerHTML = "";
 
   for (let i = 0; i < info.length; i++) {
     let newLi = document.createElement("li");
     newLi.innerHTML = `<a target="_blank" href=${info[i].link}> ${info[i].name} </a>`;
-    getUl.appendChild(newLi);
+    document.getElementById("infoList").appendChild(newLi);
   }
 }
 
 // event listener voor de radio buttons
-radioButton.forEach((button) => {
+document.querySelectorAll(".selectButton").forEach((button) => {
   button.addEventListener("change", () => {
     fetchData(event.target.value);
   });
